20ee66e3da2259196c7be7c74933b1f3daa61ab4
Nathalie <nathalievzm@hotmail.com>
2021-06-12 16:03:20 -0400

    prueba registro 2

--- src/main/webapp/public/scripts/register.js
+++ src/main/webapp/public/scripts/register.js
@@ -15,8 +15,15 @@ const enviarRegistro = () => {
     }).then(response => {
         return response.json();
     }).then(datos => {
-        console.table(datos);
+        console.table(datos.status);
+        //redireccionar(datos.status);
     })
+}
+
+const redireccionar = (status) => {
+    if(status = 200) {
+        window.location.href = "http://localhost:8080";
+    }
 }
 
 boton_registro.onclick = enviarRegistro;
