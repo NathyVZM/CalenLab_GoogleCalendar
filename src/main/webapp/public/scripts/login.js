65fd8e202322bb2fcabeb4ff73077e59d2c12b46
Nathalie <nathalievzm@hotmail.com>
2021-06-12 18:56:06 -0400

    prueba login

---
+++ src/main/webapp/public/scripts/login.js
@@ -0,0 +1,31 @@
+//login.js
+
+let formulario_login = document.getElementById("formulario-login");
+let boton_login = document.getElementById("boton-login");
+
+
+const enviarLogin = () => {
+    let formulario = new FormData(formulario_login);
+    for(let value of formulario.values()){
+        console.log(value);
+    }
+
+    fetch("http://localhost:8080/LoginUsuario", {
+        method: "POST",
+        body: formulario
+    }).then(response => {
+        return response.json();
+    }).then(datos => {
+        console.table(datos);
+        redireccionar(datos.status);
+    })
+}
+
+const redireccionar = (status) => {
+    if(status == 200) {
+        window.location.href = "http://localhost:8080";
+        //setTimeout( function() {window.location.href = "http://localhost:8080/public/views/registroRedireccion.html"}, 3000);
+    }
+}
+
+boton_login.onclick = enviarLogin;
